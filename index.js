9d0661f80fbd2025da475f43b3aa951a9857d818
Sofia Abilene Campos Hernandez <99153181+SofiaCamposH@users.noreply.github.com>
2025-03-23 08:37:49 -0600

    Api_real_time

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Encuesta = require('./models/encuesta');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/encuestasAutobus', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+//Crear encuestas 
+app.post('/encuestas', async (req, res) => {
+    try {
+      const encuesta = new Encuesta(req.body);
+      await encuesta.save();
+      res.status(201).send(encuesta);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  });
+
+  //obtener todas las encuestas 
+  app.get('/encuestas', async (req, res) => {
+    try {
+      const encuestas = await Encuesta.find({});
+      res.send(encuestas);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+
+  //Obtener promedios por Id de camion 
+  app.get('/promedios/:idCamion', async (req, res) => {
+    try {
+      const encuestas = await Encuesta.find({ idCamion: req.params.idCamion });
+  
+      if (encuestas.length === 0) {
+        return res.status(404).send({ error: 'No se encontraron encuestas para este camión.' });
+      }
+  
+      const promedios = {
+        tiempoEspera: encuestas.reduce((sum, e) => sum + e.tiempoEspera, 0) / encuestas.length,
+        calificacionLlegada: encuestas.reduce((sum, e) => sum + e.calificacionCupo, 0) / encuestas.length,
+        calificacionConductor: encuestas.reduce((sum, e) => sum + e.calificacionConductor, 0) / encuestas.length,
+        calificacionConduccion: encuestas.reduce((sum, e) => sum + e.calificacionConduccion, 0) / encuestas.length,
+        servicioGeneral: encuestas.reduce((sum, e) => sum + (e.calificacionLlegada + e.calificacionConductor + e.calificacionConduccion) / 3, 0) / encuestas.length
+      };
+  
+      res.send(promedios);
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  });
+
+  // 
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
---
+++ models/encuesta.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const encuestaSchema = new mongoose.Schema({
+  idCamion: { type: String, required: true },
+  tiempoEspera: { type: Number, required: true },
+  calificacionCupo: { type: Number, min: 1, max: 5, required: true },
+  calificacionConductor: { type: Number, min: 1, max: 5, required: true },
+  calificacionConduccion: { type: Number, min: 1, max: 5, required: true },
+  horaRespuesta: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Encuesta', encuestaSchema);
